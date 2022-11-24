bc4033444154f4c2f7e44931f46f8c5c216eb93b
Luis Pereira <36925029+LuisPereira23@users.noreply.github.com>
2022-11-24 15:38:33 +0000

    updated cameras to manual. Added model train

--- Automate.py
+++ Automate.py
@@ -9,33 +9,15 @@ colors = {
     ,"LightSalmon":(1,0.63,0.48,1)
     ,"Gold":(1,0.843,0,1)
     }
-    
-camera_locations = []
 
 number = 0
 
-def build_camera_locations(start_location=(0,-3.43,2),step=0.1):
-    current = step
-    max_step = 0.4
-    camera_locations.append(start_location)
-    while current < max_step:
-        camera_locations.append((start_location[0] + current, start_location[1], start_location[2]))
-        camera_locations.append((start_location[0] - current, start_location[1], start_location[2]))
-        camera_locations.append((start_location[0], start_location[1], start_location[2] + current))
-        camera_locations.append((start_location[0], start_location[1],start_location[2] - current))
-        camera_locations.append((start_location[0] + current, start_location[1], start_location[2] + current))
-        camera_locations.append((start_location[0] + current, start_location[1], start_location[2] - current))
-        camera_locations.append((start_location[0] - current, start_location[1], start_location[2] - current))
-        camera_locations.append((start_location[0] - current, start_location[1], start_location[2] + current))
-        current = current + step 
-     
-    print(camera_locations)
-    #print(len(camera_locations)) 25
 
-def render(number):
+def render(number, camera):
     output_dir = r'C:\Users\Krow\Documents\Uni\PI\renders\test'
     output_file_pattern_string = 'test%d.jpg'
     
+    bpy.context.scene.camera = camera
     bpy.context.scene.render.filepath = os.path.join(output_dir, (output_file_pattern_string % number))
     bpy.ops.render.render(write_still = True)
     
@@ -44,12 +26,12 @@ def turn_off_lights():
       for light in lights:
           hide_in_render(light)
 
-def loop_light():
+def loop_light(camera):
     global number
     lights = get_objects_from_collection("Lights")
     for light in lights:
         show_in_render(light)
-        render(number)
+        render(number, camera)
         number = number + 1
         hide_in_render(light)
 
@@ -57,26 +39,14 @@ def change_color(color):
     obj = bpy.context.object
     bpy.data.materials["Material"].node_tree.nodes["Principled BSDF"].inputs[0].default_value = color
     
-def update_camera(camera, location):
-    
-    focus_point = point=bpy.data.objects["Armature"].location + mathutils.Vector((-0.1,0,1.5))
-    
-    camera.location = location
-    bpy.context.view_layer.update()
-    
-    looking_direction = camera.location - focus_point
-    rot_quat = looking_direction.to_track_quat('Z', 'Y')
-    camera.rotation_euler = rot_quat.to_euler()
-    
   
 def main_loop():
     turn_off_lights()
-    build_camera_locations()
+    cameras = get_objects_from_collection("Cameras")
     for color in colors:
-        for location in camera_locations:
-            update_camera(bpy.data.objects['Camera'],location)
+        for camera in cameras:
             change_color(colors[color])
-            loop_light()
+            loop_light(camera)
     
 
 main_loop()
